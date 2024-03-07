8d5e7d4dbbffbd1a9d757c7749826bd122651117
Nataniel Cesar <cesardasilvanataniel@gmail.com>
2024-03-07 15:06:46 -0300

    configuration of angular with routes

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service/email/email-input.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from 'src/app/models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailInputService {
+
+  email: string = "";
+  private readonly API = 'api/user';
+
+  constructor(private httpClient: HttpClient) { }
+  
+ 
+
+  saveUser(user: User) {
+    return this.httpClient.post<User>(this.API, user);
+  }
+
+  
+}
