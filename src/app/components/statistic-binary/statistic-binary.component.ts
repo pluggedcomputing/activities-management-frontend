933d3b4dcdda65f80d21b9a47438b81617022f57
Nataniel Cesar <cesardasilvanataniel@gmail.com>
2024-03-11 21:00:09 -0300

    initial filter for search

--- src/app/components/statistic-binary/statistic-binary.component.ts
+++ src/app/components/statistic-binary/statistic-binary.component.ts
@@ -10,8 +10,9 @@ import { ResponsesBinaryService } from 'src/app/service/responses-binary/respons
 export class StatisticBinaryComponent implements OnInit {
 
   questions: any[] = [];
-
-  
+  filteredQuestions: any[] = [];
+  searchTerm: string = '';
+  selectedFilter: string = '';
 
   constructor(private responseBinary: ResponsesBinaryService) { }
   
@@ -19,6 +20,8 @@ export class StatisticBinaryComponent implements OnInit {
     this.responseBinary.getAllQuestion().subscribe(
       (data: any) => {
         this.questions = data;
+        // Ao receber os dados, atualize as perguntas filtradas inicialmente
+        this.filterQuestions();
       },
       error => {
         console.error('Erro ao buscar detalhes da atividade:', error);
@@ -28,7 +31,23 @@ export class StatisticBinaryComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllQuestions();
-  
   }
 
+  // Método para filtrar perguntas com base no termo de pesquisa e no filtro selecionado
+  filterQuestions() {
+    if (this.selectedFilter && this.searchTerm.trim() !== '') {
+      this.filteredQuestions = this.questions.filter(question =>
+        question[this.selectedFilter].toString().toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredQuestions = this.questions;
+    }
+  }
+
+  // Método para limpar o campo de pesquisa e redefinir os filtros
+  clearSearch() {
+    this.searchTerm = '';
+    this.selectedFilter = '';
+    this.filterQuestions(); // Reaplicar os filtros após limpar
+  }
 }
