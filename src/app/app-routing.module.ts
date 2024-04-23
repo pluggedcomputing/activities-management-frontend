f08668564e82e1032ef78a16ef0cf2029f2a3d2b
Nataniel Cesar <cesardasilvanataniel@gmail.com>
2024-04-23 18:17:07 -0300

    astetics updates in:
    - New Navegation Menu
    - Menu: Todas as respostas
    - Menu: Pesquisar usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { StatisticBinaryComponent } from './components/binary/statistic-binary/statistic-binary.component'
-import { HomeBinaryComponent } from './components/binary/home-binary/home-binary.component';
 import { SearchUserComponent } from './components/binary/search-user/search-user.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'statistics-binary', component: StatisticBinaryComponent},
-  {path: 'home-binary', component: HomeBinaryComponent},
   {path: 'search-user', component: SearchUserComponent}
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatButtonModule} from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { StatisticBinaryComponent } from './components/binary/statistic-binary/statistic-binary.component';
 import { HomeComponent } from './components/home/home.component';
-import { HomeBinaryComponent } from './components/binary/home-binary/home-binary.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+
+
+
+
+
+
+
 import { SearchUserComponent } from './components/binary/search-user/search-user.component';
 
 @NgModule({
@@ -18,18 +31,27 @@ import { SearchUserComponent } from './components/binary/search-user/search-user
     AppComponent,
     StatisticBinaryComponent,
     HomeComponent,
-    HomeBinaryComponent,
-    SearchUserComponent
+    SearchUserComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatDatepickerModule
+    
+    
     
   ],
   providers: [],
