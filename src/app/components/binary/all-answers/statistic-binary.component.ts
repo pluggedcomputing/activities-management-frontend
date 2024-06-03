e9c42e3b757e771c55413e7451dda1cf127eb22a
Nataniel Cesar <cesardasilvanataniel@gmail.com>
2024-06-03 18:25:01 -0300

    Feature: adicionado filtro de todas as questões com data

--- src/app/components/binary/all-answers/statistic-binary.component.ts
+++ src/app/components/binary/all-answers/statistic-binary.component.ts
@@ -1,74 +1,86 @@
-// Importando dependências necessárias do Angular
 import { Component, OnInit } from '@angular/core';
 import { ResponsesBinaryService } from 'src/app/service/response/responses-binary.service';
 
-
-// Componente Angular para exibição de estatísticas de questões binárias
 @Component({
-  selector: 'app-statistic-binary', // Seletor do componente
-  templateUrl: './statistic-binary.component.html', // Template HTML associado ao componente
-  styleUrls: ['./statistic-binary.component.css'] // Estilos CSS associados ao componente
+  selector: 'app-statistic-binary',
+  templateUrl: './statistic-binary.component.html',
+  styleUrls: ['./statistic-binary.component.css']
 })
 export class StatisticBinaryComponent implements OnInit {
-
-  // Variáveis para armazenar todas as questões, questões filtradas, termo de pesquisa e ordem selecionada
   questions: any[] = [];
   filteredQuestions: any[] = [];
   searchTerm = '';
-  selectedOrder = 'phaseActivity';
+  selectedOrder = 'phaseActivity'; 
   dataOn = true;
-  startDate = null;
-  endDate = null;
+  startDate: Date | null = null;
+  endDate: Date | null = null;
 
   constructor(private responseBinary: ResponsesBinaryService) { }
 
-  // Método do ciclo de vida do componente, chamado após a inicialização do componente
   ngOnInit(): void {
-    this.loadAllQuestions(); // Carrega todas as questões ao inicializar o componente
+    this.loadAllQuestions();
   }
 
-  // Método para carregar todas as questões 
   loadAllQuestions(): void {
     this.responseBinary.getAllQuestion().subscribe(
-      // Callback de sucesso
       (data: any) => {
         this.questions = data;
-        this.orderQuestions(); // Ordena as questões por data após o carregamento
-        this.filterQuestions(); // Aplica os filtros
+        this.orderQuestions();
+        this.filterQuestions();
       },
-      // Callback de erro
       error => {
         console.error('Erro ao buscar detalhes da atividade:', error);
       }
     );
   }
 
-  // Método para filtrar as questões com base no termo de pesquisa e na ordem selecionada
+  
   filterQuestions(): void {
     if (this.selectedOrder && this.searchTerm.trim() !== '') {
-      // Filtra as questões com base no termo de pesquisa e na ordem selecionada
       this.filteredQuestions = this.questions.filter(question =>
+        question[this.selectedOrder] &&
         question[this.selectedOrder].toString().toLowerCase().includes(this.searchTerm.toLowerCase())
       );
     } else {
-      this.filteredQuestions = this.questions.slice(); // Se não houver termo de pesquisa, retorna todas as questões
+      this.filteredQuestions = this.questions.slice();
+    }
+  
+    if (this.selectedOrder === "dateResponse" && this.startDate !== null && this.endDate !== null) {
+      this.responseBinary.getAllQuestionWithDate(this.startDate, this.endDate).subscribe(
+        (data: any) => {
+          this.questions = data;
+          this.orderQuestions();
+          // Aplicar novamente o filtro de searchTerm após atualizar as questões
+          if (this.searchTerm.trim() !== '') {
+            this.filteredQuestions = this.questions.filter(question =>
+              question[this.selectedOrder] &&
+              question[this.selectedOrder].toString().toLowerCase().includes(this.searchTerm.toLowerCase())
+            );
+          } else {
+            this.filteredQuestions = this.questions.slice();
+          }
+        },
+        error => {
+          console.error('Erro ao buscar detalhes da atividade:', error);
+        }
+      );
+    }
+  }
+  
+
+  orderQuestions(): void {
+    if (this.selectedOrder) {
+      this.questions.sort((a, b) => {
+        const dateA = new Date(a[this.selectedOrder]);
+        const dateB = new Date(b[this.selectedOrder]);
+        return dateB.getTime() - dateA.getTime();
+      });
     }
   }
 
-  // Método para ordenar as questões com base na ordem selecionada
-  orderQuestions(): void {
-    this.questions.sort((a, b) => {
-      // Converte as datas para objetos Date e compara para ordenar
-      const dateA = new Date(a[this.selectedOrder]);
-      const dateB = new Date(b[this.selectedOrder]);
-      return dateB.getTime() - dateA.getTime(); // Retorna a diferença entre as datas
-    });
-  }
-
-  // Método para limpar o termo de pesquisa e a ordem selecionada
   clearSearch(): void {
     this.searchTerm = '';
-    this.selectedOrder = '';
-    this.filterQuestions(); // Aplica os filtros novamente para mostrar todas as questões
+    this.selectedOrder = 'phaseActivity'; 
+    this.filterQuestions();
   }
 }
--- src/app/service/response/responses-binary.service.ts
+++ src/app/service/response/responses-binary.service.ts
@@ -13,9 +13,14 @@ export class ResponsesBinaryService {
 
   constructor(private httpClient: HttpClient) { }
 
-  // Pega todas as questões.
+  // Pega todas as respostas.
   getAllQuestion(){
     return this.httpClient.get(`${this.API}`);
+  }
+
+   // Pega todas as respostas com datas específicas.
+   getAllQuestionWithDate(startDate: Date, endDate: Date){
+    return this.httpClient.get<Question[]>(`${this.API}?startDate=${startDate}&endDate=${endDate}`);
   }
 
   // Pega todas as respostas de uma questão específica
@@ -38,23 +43,24 @@ export class ResponsesBinaryService {
     return this.httpClient.get<QuestionStatistics>(`${this.API}/getStatisticsResponse?phase=${phase}&activity=${activity}&startDate=${startDate}&endDate=${endDate}`)
   }
 
-    // Pega todas as questões de um usuário específico.
-    getQuestionsOfUser(userID: string, idApp: string) {
-      return this.httpClient.get<Question[]>(`${this.API}/getUniqueUser?userID=${userID}&idApp=${idApp}`);
-    }
+  // Pega todas as questões de um usuário específico.
+  getQuestionsOfUser(userID: string, idApp: string) {
+    return this.httpClient.get<Question[]>(`${this.API}/getUniqueUser?userID=${userID}&idApp=${idApp}`);
+  }
   
-    // Pega todas as questões de um usuário específico e em uma data específica.
-    getQuestionsOfUserWithDate(userID: string, idApp: string, startDate: Date, endDate: Date) {
-      return this.httpClient.get<Question[]>(`${this.API}/getUniqueUser?userID=${userID}&idApp=${idApp}&startDate=${startDate}&endDate=${endDate}`);
-    }
+  // Pega todas as questões de um usuário específico e em uma data específica.
+  getQuestionsOfUserWithDate(userID: string, idApp: string, startDate: Date, endDate: Date) {
+    return this.httpClient.get<Question[]>(`${this.API}/getUniqueUser?userID=${userID}&idApp=${idApp}&startDate=${startDate}&endDate=${endDate}`);
+  }
   
-    // Pega todas as estastísticas de um usuário específico.
-    getStatisticsUser(userID: string, idApp: string){
-      return this.httpClient.get<UserStatistics>(`${this.API}/getStatisticsUser?userID=${userID}&idApp=${idApp}`);
-    }
-  
-    // Pega todas as estastísticas de um usuário específico em uma data específica.
-    getStatisticsUserWithDate(userID: string, idApp: string, startDate: Date, endDate: Date){
-      return this.httpClient.get<UserStatistics>(`${this.API}/getStatisticsUser?userID=${userID}&idApp=${idApp}&startDate=${startDate}&endDate=${endDate}`);
-    }
+  // Pega todas as estastísticas de um usuário específico.
+  getStatisticsUser(userID: string, idApp: string){
+    return this.httpClient.get<UserStatistics>(`${this.API}/getStatisticsUser?userID=${userID}&idApp=${idApp}`);
+  }
+    
+  // Pega todas as estastísticas de um usuário específico em uma data específica.
+  getStatisticsUserWithDate(userID: string, idApp: string, startDate: Date, endDate: Date){
+    return this.httpClient.get<UserStatistics>(`${this.API}/getStatisticsUser?userID=${userID}&idApp=${idApp}&startDate=${startDate}&endDate=${endDate}`);
+  }
+
 }
